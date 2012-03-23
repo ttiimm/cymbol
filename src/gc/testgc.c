f5fef0c48ffac807c2e46b22be3ea7bc4d3f6129
Tim Likarish <likarish@gmail.com>
2012-03-23 00:39:29 -0700

    test rig for running garbage collector

--- src/gc/testgc.c
+++ src/gc/testgc.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-  /*  struct TypeDescriptor[] types = {string_type, User_type};
-      gc_init(types);*/
+  struct TypeDescriptor[] types = {string_type, User_type};
+  gc_init(types);
   return 0;
 }
